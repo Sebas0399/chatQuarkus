9c21b833bc8745b387f32053e8f629af4321cad1
Sebas-Master <aarguello@konnet.app>
2025-08-12 16:42:29 -0500

    first commit

---
+++ src/main/java/websocket/ChatWebSocket.java
@@ -0,0 +1,41 @@
+package websocket;
+
+import io.quarkus.logging.Log;
+import io.quarkus.websockets.next.OnClose;
+import io.quarkus.websockets.next.OnOpen;
+import io.quarkus.websockets.next.OnTextMessage;
+import io.quarkus.websockets.next.WebSocket;
+import io.quarkus.websockets.next.WebSocketConnection;
+import jakarta.inject.Inject;
+
+@WebSocket(path = "/chat/{username}")
+public class ChatWebSocket {
+
+	// Declare the type of messages that can be sent and received
+	public enum MessageType {USER_JOINED, USER_LEFT, CHAT_MESSAGE}
+	public record ChatMessage(MessageType type, String from, String message) {
+	}
+
+	@Inject
+	WebSocketConnection connection;
+
+	@OnOpen(broadcast = true)
+	public ChatMessage onOpen() {
+		Log.info("onOppen: " + connection);
+
+		return new ChatMessage(MessageType.USER_JOINED, connection.pathParam("username"), null);
+	}
+
+	@OnClose
+	public void onClose() {
+		ChatMessage departure = new ChatMessage(MessageType.USER_LEFT, connection.pathParam("username"), null);
+		connection.broadcast().sendTextAndAwait(departure);
+	}
+
+	@OnTextMessage(broadcast = true)
+	public ChatMessage onMessage(ChatMessage message) {
+		Log.info("onMessage: " + message);
+		return message;
+	}
+
+}
